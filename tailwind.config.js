4569a18bb9034ded8112dbf49fa27455eacf368c
Christer Olsen <130916986+christerolsen@users.noreply.github.com>
2024-10-23 21:11:12 +0200

    Apply minor fixed for TailwindCSS and Global Styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-module.exports = {
+export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        lato: ["Lato", "sans-serif"],
-        roboto: ["Roboto", "sans-serif"],
+        lato: ['"Lato"', "sans-serif"],
+        roboto: ['"Roboto"', "sans-serif"],
       },
       fontSize: {
         h1: ["48px", { lineHeight: "52.8px" }],
@@ -13,13 +13,13 @@ module.exports = {
         h4: ["28px", { lineHeight: "30.8px" }],
         h5: ["24px", { lineHeight: "26.4px" }],
         h6: ["20px", { lineHeight: "22px" }],
-        "body-large": ["18px", { lineHeight: "25.2px" }],
-        "body-medium": ["16px", { lineHeight: "22.4px" }],
-        "body-regular": ["14px", { lineHeight: "19.6px" }],
-        "body-small": ["12px", { lineHeight: "16.8px" }],
+        "body-large": ["20px", { lineHeight: "25.2px" }],
+        "body-medium": ["18px", { lineHeight: "22.4px" }],
+        "body-regular": ["16px", { lineHeight: "19.6px" }],
+        "body-small": ["14px", { lineHeight: "16.8px" }],
       },
       colors: {
-        primary: "#2d9cdb", // Soft blue
+        primary: "#4A90E2", // Soft blue
         secondary: "#50e3c2", // Minty green
         accent: "#f5a623", // Warm orange
         info: "#2D9CDB", // Light blue for info state
@@ -44,11 +44,13 @@ module.exports = {
         xxxl: "80px", // Triple extra large
       },
       padding: {
-        // Custom padding based on font size rules
-        "btn-x": "1rem", // This will be for top & bottom (1 x font size)
-        "btn-y": "5rem", // This will be for left & right (5 x font size)
+        "btn-y": "1rem", // Top & bottom padding
+        "btn-x": "5rem", // Left & right padding
       },
     },
   },
   plugins: [],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  },
 };
